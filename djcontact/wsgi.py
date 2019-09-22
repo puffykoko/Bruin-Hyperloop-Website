c3347fb38cf80415c71890cc1bc7592722be515e
Matthew Ko <mk9917@yahoo.com>
2019-09-22 15:03:43 -0700

    secured api key, updated title, fixed image

--- djcontact/wsgi.py
+++ djcontact/wsgi.py
@@ -12,5 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djcontact.settings')
+os.environ['DJANGO_DEBUG'] = 'False'
 
 application = get_wsgi_application()
