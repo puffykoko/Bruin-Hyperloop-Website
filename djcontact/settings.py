5a0af1521e3e385ed975ff990835dfac44dc7fe7
Matthew Ko <mk9917@yahoo.com>
2019-09-24 22:00:07 -0700

    Revert "fixed settings"
    This reverts commit f1aa1d251b18704f6c3e7b5a971df8da3c2a6998.

--- djcontact/settings.py
+++ djcontact/settings.py
@@ -118,10 +118,6 @@ STATICFILES_DIRS = [
     os.path.join(REACT_APP_DIR, 'build', 'static'),
 ]
 
-# Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
 
 #For Devlopment Only
     #CORS_ORIGIN_ALLOW_ALL = True
